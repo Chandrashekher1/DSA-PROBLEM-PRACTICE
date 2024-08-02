0f98b5a217ccedc99fb42d20c30bdfce93da11de
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-08-02 23:11:07 +0530

    steps Count

---
+++ Recursion/Problems/src/com/CP03/EasyProblem/StepsCount.java
@@ -0,0 +1,17 @@
+package com.CP03.EasyProblem;
+
+public class StepsCount {
+    public static int Steps(int num, int steps){
+        if (num == 0){
+            return steps;
+        }
+        if (num %2 == 0){
+            return Steps(num/2, steps+1);
+        }
+        return Steps(num -1, steps+1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Steps(14,0));
+    }
+}

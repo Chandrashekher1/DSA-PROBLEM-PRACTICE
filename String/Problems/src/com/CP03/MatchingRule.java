fdf7c94f31cf88282eab9e62bc607c98aa06a14c
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-08-18 19:47:45 +0530

    matching character

---
+++ String/Problems/src/com/CP03/MatchingRule.java
@@ -0,0 +1,25 @@
+package com.CP03;
+
+//https://leetcode.com/problems/count-items-matching-a-rule/description/
+
+import java.util.List;
+
+public class MatchingRule {
+    public int countMatches(List<List<String>> items,String ruleKey, String ruleValue) {
+        int count = 0;
+
+        for (int i=0; i<items.size(); i++){
+            if (ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)){
+                count++;
+            }
+            if (ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)){
+                count++;
+            }
+            if (ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+}

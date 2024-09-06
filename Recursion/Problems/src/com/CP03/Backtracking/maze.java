c933b4261cac57200851e31f0cc10bc3a2e9d86a
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-09-06 22:11:38 +0530

    Backtracking

---
+++ Recursion/Problems/src/com/CP03/Backtracking/maze.java
@@ -0,0 +1,17 @@
+package com.CP03.Backtracking;
+
+public class maze {
+    public static int count(int r, int c){
+        if (r == 1 || c == 1){
+            return 1;
+        }
+        int left = count(r-1, c);
+        int right = count(r,c-1);
+
+        return left + right;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(count(3,3));
+    }
+}
